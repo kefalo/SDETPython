21dd57413759bc6a72371180c1e557c08f9a89d3
stefa <marinkovics92@gmail.com>
2023-11-01 22:27:51 +0100

    changed course to SDET on YT DAY1 python3, variables etc

---
+++ Day1(SDET)/Comments.py
@@ -0,0 +1,8 @@
+# single line comment  use ctrl + /
+
+""" print("some text")
+print("some text")
+print("some text")
+print("some text")
+print("some text") - multiline comments three double quotes or single quotes to be green color
+                    """
---
+++ Day1(SDET)/DataTypes.py
@@ -0,0 +1,26 @@
+'''
+A variable is nothing but reserved space in memory for some value.
+Variables are used to store the data.
+Memory allocated when the values are stored in variables.
+Every variable must have a data type.
+'''
+
+# Dynamically typed programming language
+
+# Data types
+#Text: str
+#Numeric: int, float
+#Sequence: list, tuple
+#Mapping: dict
+#Set: set
+#Boolean: bool
+
+intVariabla = 1
+floatVariabla = 10.11
+stringVariabla = "StringVariabla"
+booleanVariabla = True
+
+print(type(intVariabla))
+print(type(floatVariabla))
+print(type(stringVariabla))
+print(type(booleanVariabla))
